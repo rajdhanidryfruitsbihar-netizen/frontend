ebfdba7e92d41329d788bbf3ce280f43e8fc0835
rajdhanidryfruits <rajdhanidryfruitsbihar@gmail.com>
2026-08-14 17:53:43 +0530

    fix vercel react routing

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://backend-livid-sigma-62.vercel.app/api";
 
 console.log("API URL:", API_URL);
 
